14db86adae6cf2edd74661c2cd2c3bf27c18d96d
Nazariy <ngrebn@mail.ru>
2025-04-23 17:49:59 +0300

    1.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,35 @@
-// @todo: Темплейт карточки
+'use strict'
 
-// @todo: DOM узлы
+const unorderList = document.querySelector('.places__list')
 
-// @todo: Функция создания карточки
+const createCard = function(data , callbackDelete) {
+    const cardTemplate = document.querySelector('#card-template').content
+    const cardElement = cardTemplate.querySelector(".card").cloneNode(true)
 
-// @todo: Функция удаления карточки
+    cardElement.querySelector('.card__image').src = data.link
+    cardElement.querySelector('.card__image').alt = `Фото локации: ${data.name}`
+    cardElement.querySelector('.card__title').textContent = data.name
 
-// @todo: Вывести карточки на страницу
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function() {
+        return callbackDelete(cardElement)
+    })
+
+    cardElement.querySelector('.card__like-button').addEventListener('click' , function(evt) {
+    return evt.target.classList.toggle('card__like-button_is-active')
+    })
+
+    return cardElement
+}
+
+const addCard = function() {
+    initialCards.forEach(function(element) {
+        const setCard = createCard(element , deleteCard)
+        return unorderList.append(setCard)
+    })
+}
+
+const deleteCard = function(element) {
+   return element.remove()
+}
+
+addCard()
