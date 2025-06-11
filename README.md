8501ec3f91779b372c55ded1a4f1640251dd94c6
Nazariy <ngrebn@mail.ru>
2025-06-11 15:53:49 +0300

    1.4

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Проектная работа Mesto
-
 https://podicalin.github.io/mesto-project-ff/
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./pages/index.css"
 import { initialCards } from "./components/cards.js"
 import { openModal , closeModal } from "./components/modal.js"
 import { createCard , clicklikeButton ,  deleteCard } from "./components/card.js"
+import { enableValidation , resetValidation } from "./components/validation.js"
 //Список карточек
 const unorderList = document.querySelector('.places__list')
 //Попапы
@@ -99,82 +100,14 @@ function handleAddCardForm(evt) {
 }
 newPlaceForm.addEventListener('submit', handleAddCardForm)
 
-// Функция, которая показывает сообщение об ошибке рядом с полем ввода
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.add('popup__input_type_error')
-  errorElement.textContent = errorMessage
-  errorElement.classList.add('popup__input-error_active')
-}
+const validConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__button',
+  inactiveButtonClass: 'popup__button_inactive',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active'
+} 
 
-// Функция, которая скрывает сообщение об ошибке, если пользователь всё ввёл правильно
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.remove('popup__input_type_error')
-  errorElement.classList.remove('popup__input-error_active')
-  errorElement.textContent = ''
-}
-
-// Проверяет корректность введённых данных в одном поле
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage)
-  } else {
-    hideInputError(formElement, inputElement)
-  }
-}
-
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement)=> {
-    return !inputElement.validity.valid
-  })
-  }
-
-const toggleButtonState = (inputList, buttonElement) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
-    buttonElement.disabled = true;
-    buttonElement.classList.add('popup__button-inactive')
-  } else {
-    // иначе сделай кнопку активной
-    buttonElement.disabled = false;
-    buttonElement.classList.remove('popup__button-inactive')
-  }
-}
-
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'))
-   const buttonElement = formElement.querySelector('.popup__button')
-   toggleButtonState(inputList, buttonElement)
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement)
-      toggleButtonState(inputList, buttonElement)
-    })
-  })
-}
-//Функция валилации
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__form'))
-  formList.forEach((formElement) => {
-    setEventListeners(formElement)
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault()
-    })
-  })
-}
-enableValidation()
-//Удаление ошибок валидации
-const resetValidation = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-  const buttonElement = formElement.querySelector('.popup__button');
-
-  inputList.forEach((inputElement) => {
-    hideInputError(formElement, inputElement)
-    inputElement.value = ''
-  })
-  
-  toggleButtonState(inputList, buttonElement);
-}
+enableValidation(validConfig)
 
